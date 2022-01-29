9f5022069038e94dcf3b75a6634ed0ca1b798f33
Andrey <civka.jp@gmail.com>
2022-01-29 20:36:13 +0300

    Урок_3

--- src/App.js
+++ src/App.js
@@ -2,33 +2,45 @@ import React from 'react';
 import './App.scss';
 import Input from './Input';
 import Message from './MessageList'
+import AlignItemsList from './List'
 import { useState, useEffect } from 'react';
+import AddReactionOutlinedIcon from '@mui/icons-material/AddReactionOutlined';;
 
 function App() {
   const [messageBox, setMessageBox] = useState([]);
+  const [listBox, setListBox] = useState([]);
   let [lineText, setLineText] = useState('');
 
   useEffect(()=>{
     if(((messageBox.length+1)%2) === 0){
-      setMessageBox([...messageBox,
-        {
+      setMessageBox([{
             text: '^-^', 
             autor: 'bot'
-        }]);
+        },
+        ...messageBox]);
     }
   },[messageBox])
 
   const updateMessage = () => {
-    // const text = document.querySelector('.input-text').value;
     if(lineText){
-    setMessageBox([...messageBox,
-                  {
+    setMessageBox([{
                       text: lineText, 
                       autor: 'me'
-                  }]);
-                  document.querySelector('.input-text').value = '';
+                  },
+                  ...messageBox]);
+                  // document.querySelector('.input-text').value = '';
+                  lineText = '';
                   setLineText ('');
                 }
+  }
+
+  const addPersonChat = () => {
+    setListBox([{
+                  Name: 'Ivan',
+                  Status: 'Online',
+                  Avatar: "/static/images/avatar/1.jpg"
+              },
+              ...listBox]);
   }
 
   const lineUpdate =(event) =>{
@@ -37,54 +49,40 @@ function App() {
 
   return (
     <div className="App-header App">
-      <div className="Message-box"> 
+      
+      <div className="messagePage"> 
+        <div className='listPage'>
+            <button onClick={addPersonChat} className='newPerson'>
+              <AddReactionOutlinedIcon />
+             <p>Add new chat</p>
+            </button>
+            <div className="boxList">
+              {listBox.map((person, index)=>(
+                <div key={index} className='person'>
+                  <AlignItemsList 
+                      addName = {person.Name}
+                      addStatus = {person.Status}
+                      addFoto = {person.Avatar}
+                  />
+                </div>
+              ))}
+            </div>
+        </div>
+        <div className="Message-box"> 
           {messageBox.map((send, index)=>(
             <div key={index} style={{width: '100%'}}>
-              <Message outputText = {send.text} outputAutor = {send.autor} />
+              <Message 
+                       outputText = {send.text} 
+                       outputAutor = {send.autor} 
+                    />
             </div>
           ))}   
-          </div>
-      <Input eventClick = {updateMessage} eventLineUpdate = {lineUpdate}/>
+        </div>
+        
+      </div>
+      <Input eventClick = {updateMessage} eventLineUpdate = {lineUpdate} inputTextLine = {lineText}/>
     </div>
   );
 }
-
-// class App extends React.Component{
-//  constructor(props){
-//   super(props);
-//   this.MessageBox = [];
- 
-//      this.state = {
-//        count: 0,
-//        textPrint:'',
-//        autor: '',
-//      }
-//  }
-
-// inputMessage = () =>{
-//   const text = document.querySelector('.input-text').value;
-//   this.setMessageBox({
-//                   textPrint: text,
-//                   autor: 'me'
-//   })
-//   document.querySelector('.input-text').value ='';  
-// }
-
-//   render(){
-//             return (
-//                     <div className="App-header App">
-//                         <div className="Message-box">
-//                         {this.MessageBox.map((send)=>(
-//                             <Message outputText = {this.state.textPrint} outputAutor ={this.state.autor}/>
-//                         ))}     
-//                         </div>
-//                         {/* <MessageList outputList = {this.state.textPrint}/> */}
-//                         <Input some_event = {this.inputMessage} />
-//                         {/* <span className='counter'>{count}</span>
-//                         <button onClick={updateCount} className='counter-button'>Click!</button> */}
-//                     </div>
-//                     ); 
-//           }  
-// }
 
  export default App;
